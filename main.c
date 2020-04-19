14e4394cbdb90fec858f6b38ab5fe29bacccf8fd
Hwang suhwan <hsh010126@gmail.com>
2020-04-19 12:15:13 +0900

    add serching function in manaer.c and main.c

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ printf("2. 과자 정보 출력\n");
 printf("3. 과자 정보 수정\n");
 printf("4. 과자 정보 삭제\n");
 printf("5. 과자 정보 저장\n");
-//printf("6. 과자 정보 검색\n");
+printf("6. 과자 정보 검색\n");
 printf("0. 종료\n");
 printf("\n메뉴 번호를 입력해주세요.");
 scanf("%d",&menu);
@@ -68,9 +68,9 @@ while(1){
     else if(menu==5){
         saveProduct(p,count);
 	}
-/*else if(menu==6){
+   else if(menu==6){
         searchProduct(p,count);
-    }*/
+    }
     else if(menu==0){
         break;
     }
--- manager.c
+++ manager.c
@@ -40,3 +40,71 @@ int loadProduct(product *p[]){
         return i-1;
     }
 }
+void searchProduct(product *p[],int count){
+	int check=0;
+	int mode;
+	printf("원하는 검색방식은? (1.이름 2.무게 3.가격0. 취소)");
+	scanf("%d",&mode);
+	getchar();
+	if(mode==0){
+        	printf("취소됨!\n");
+	}
+	else if(mode==1){
+		check = sname(p,count);
+	}
+	else if (mode ==2){
+		check = sweight(p,count);
+	}
+	else if (mode ==3){
+		check = sprice(p,count);
+	}
+	else{
+		printf("잘못된 입력 입니다.\n");
+	}
+	if(check==0)printf("==>검색 결과 없음\n");
+}
+int sname(product*p[],int count){
+	int check=0;
+	char src[20];
+	printf("검색할 이름은?");
+	scanf("%[^\n]",src);
+	printf("   무게  가격  표준가격 별점 이름\n");
+	for(int i=0;i<count;i++){
+		if(p[i]->price!=-1)
+		if(strstr(p[i]->name,src)){
+			readProduct(p[i]);	
+			check=1;
+		}
+	}
+	return check;	
+}
+int sweight(product*p[],int count){
+	int check=0;
+	int sr=0;
+	printf("검색할 무게는?");
+	scanf("%d",&sr);
+	printf("이름 무게 가격 표준가격 별점\n");	
+	for(int i=0;i<count;i++){
+		if(p[i]->price!=-1)
+		if(sr==p[i]->weight){
+			readProduct(p[i]);	
+			check=1;
+		}
+	}
+	return check;	
+}
+int sprice(product*p[],int count){
+	int check=0;
+	int sr=0;
+	printf("검색할가격은?");
+	scanf("%d",&sr);
+	printf("이름 무게 가격 표준가격 별점\n");	
+	for(int i=0;i<count;i++){
+		if(p[i]->price!=-1)
+		if(sr==p[i]->price){
+			readProduct(p[i]);	
+			check=1;
+		}
+	}
+	return check;	
+}
